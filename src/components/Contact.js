5b8db698b8b0714a619a02978c7eb3f0c77cdc86
Travis Turner <roboenigma@gmail.com>
2018-01-17 17:58:27 -0500

    Add contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import superagent from 'superagent';
 
 class Contact extends React.Component {
     constructor (props){
         super(props);
+    }
+    state = {
+        messageSent: false
+    }
+    componentWillMount () {
+        let messageSent = localStorage.getItem('messageSent') || false;
+        this.setState(() => {
+            return {
+                messageSent
+            }
+        });
+    }
+    onFormSubmit = (e) => {
+        localStorage.setItem('messageSent', true);
     }
     render () {
         return (
             <div id="form__container">
             <h1>Contact</h1>
             <hr className="form__rule"/>
+            {this.state.messageSent && <p id="flash__message">Message sent!</p>}
             <div id="form__info">
                 <p id="form__phone-message">
                     You can reach me by calling or sending a text message to <a href="+8593021532">859-302-1532</a>.
@@ -17,6 +33,57 @@ class Contact extends React.Component {
                     My e-mail address is <a href="mailto:travis@travisturner.io">travis@travisturner.io</a>.
                 </p>
             </div>
+            <div id="form__form-container">
+            
+                <div id="form__form-header">
+                    
+                </div>
+                <form onSubmit={this.onFormSubmit} method="POST" 
+                action="https://contact-e-mail-forward-er.herokuapp.com/">
+                    <div id="form__flex">
+                    
+                        <div className="form__row">
+                            <h3>...or send a message below</h3>
+                        </div>
+                        <div className="form__row input__row">
+                            <div className="inner__row">
+                                <div id="form__name">
+                                    <label htmlFor="name">Name:<span className="required">*</span></label>
+                                    <input type="text" name="name" required/>
+                                </div>
+                                <div id="form__email">
+                                    <label htmlFor="email">E-mail:<span className="required">*</span></label>
+                                    <input type="text" name="email" required/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="form__row input__row">
+                            <div className="inner__row">
+                                <div id="form__website">
+                                    <label htmlFor="website">Website:</label>
+                                    <input type="text" name="website" />
+                                </div>                              
+                                <div id="form__phone">
+                                    <label htmlFor="phone">Phone:</label>
+                                    <input type="text" name="phone" />
+                                </div>
+                            </div>
+                        </div> 
+                            <div className="form__row">     
+                                    <label id="your__message" htmlFor="message">Your message here:<span className="required">*</span></label>
+                            </div>
+                            <div className="form__row" id="form__row3">   
+                                <div className="inner__row">  
+                                    <textarea name="message" required></textarea>
+                                </div>
+                            </div>
+                            <div id="button__container" className="form_row">
+                                <button id="submit__button">Send</button>
+                            </div>
+                        </div>
+                    </form>
+            </div>
+              
             </div>
         )
     }
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,18 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import DashboardInfo from './DashboardInfo';
 
-const DashboardPage = () => (
-  <div>
-    <div id="dashboard__container">
-      <div id="dashboard__header">
-        <h1>Hello,  I'm Travis.</h1>
-        <h3>I'm a <strong>full stack web developer</strong> and <strong>programmer from Berea, KY.</strong></h3>
-        <h3>My focus is on <strong>JavaScript</strong> as well as its related frameworks and technologies. I'm currently looking for new projects, potential clients and web development opportunities.</h3>
-        <Link to="/contact"><button id="dashboard--button">CONTACT ME</button></Link>
+class DashboardPage extends React.Component {
+  componentWillMount () {
+    localStorage.clear();
+  }
+  render () {
+    return (
+      <div>
+        <div id="dashboard__container">
+          <div id="dashboard__header">
+            <h1>Hello,  I'm Travis.</h1>
+            <h3>I'm a <strong>full stack web developer</strong> and <strong>programmer from Berea, KY.</strong></h3>
+            <h3>My focus is on <strong>JavaScript</strong> as well as its related frameworks and technologies. I'm currently looking for new projects, potential clients and web development opportunities.</h3>
+            <Link to="/contact"><button id="dashboard--button">CONTACT ME</button></Link>
+          </div>
+        </div>
+        <DashboardInfo />
       </div>
-    </div>
-    <DashboardInfo />
-  </div>
-);
+    )
+  }
+}
 
 export default DashboardPage;
