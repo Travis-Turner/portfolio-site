2a9c0bb255e6db50cb8cb262908cd29a74f0373f
Travis Turner <roboenigma@gmail.com>
2018-01-18 08:28:01 -0500

    Add sub-headers and some language changes and corrections

--- src/components/DashboardInfo.js
+++ src/components/DashboardInfo.js
@@ -3,39 +3,42 @@ import React from 'react';
 const DashboardInfo = () => (
   <div id="dashboard__info">
     <div id="dashboard__info-left">
+        <h2 id="dashboard__info-h2">A quick introduction</h2>
         <p>
             After starting off in <strong>C</strong> and <strong>Python</strong>, I began learning&nbsp; 
-            <strong>JavaScript</strong>. It's now become my preferred language,
-            but it is by no means the only one I use.  And I'm always down to pick up something new. 
+            <strong>JavaScript</strong>. It's since become my preferred language,
+            but it is by no means the only one I use. 
         </p>
         <p>
             As a freelance web developer, I draw from an extensive toolkit to fit my client needs.
             These days, I'm working with (and loving) <strong>React</strong>.  
-            If the project calls for it, I'll incorporate <strong>Redux</strong> into the mix.  
+            If the project calls for it, I'll incorporate <strong>Redux</strong> into the mix.  I'm always down to pick up something new. 
         </p>
+        <h2>My skills and toolkit</h2>
         <p>
             In addition to <strong>JavaScript</strong> I'm also well-versed in various web tools like 
             &nbsp;<strong>HTML5</strong>, <strong>CSS3</strong> as well as <strong>Bootstrap</strong>, 
-            &nbsp;<strong>jQuery</strong>, and technologies such as <strong>AJAX</strong> and <strong>Firebase</strong>.
+            <strong>&nbsp;jQuery</strong>, and technologies such as <strong>AJAX</strong> and <strong>Firebase</strong>.
             I also have experience with <strong>Wordpress</strong>, as well as <strong>PHP</strong>.
         </p>
         <p>
             On the backend, I'm a big fan of <strong>NodeJS</strong> and <strong>MongoDB</strong>, 
-            though I am comfortable using <strong>SQL</strong> databases as well.
+            though I like using <strong>SQL</strong> databases as well.
         </p>
         <p>
             Though I am not specifically a graphic designer, I am experienced with tools such as 
             &nbsp;<strong>Adobe Illustrator</strong>, <strong>Adobe Photoshop</strong>, and 
-            most recently <strong>Sketch</strong>.  On that note, I also believe I have an eye - or more appropriately, a feel - for good <strong>UX and UI</strong> experiences.
+            <strong>Sketch</strong>.  I have an eye - or more appropriately, a feel - for strong <strong>UX and UI</strong> experiences.
         </p>
+        <h2>But wait, there's more</h2>
         <p>
-            Finally, I have professioanl experience using a myrid of video and audio editing software as well as writing for
+            Finally, I have professional experience using a myrid of video and audio editing software as well as writing for
             digital media.  I'm always looking for my next big challenge, and I would be happy to hear from you soon.
         </p>
     </div>
     <div id="dashboard__info-right">
         <p>
-            Please check out the following links:
+            Check these out:
         </p>
         <hr id="rule"/>
         <p>
@@ -48,7 +51,7 @@ const DashboardInfo = () => (
             Follow me on Twitter: <a href="https://twitter.com/_Travis_Turner" target="_blank"><i className="fa fa-twitter-square" aria-hidden="true"></i></a>
         </p>
         <p id="dashboard__info-thanks">
-            <em>Thanks.</em>
+            <em>Thanks for visiting.</em>
         </p>
         
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,10 @@ import { NavLink, Link } from 'react-router-dom';
 
 const NavBar = () => (
   <div id="navbar__container">
+  <a className="header__link" href="./TravisTurnerResume2018.pdf" target="_blank">CV</a>
     <NavLink exact className="header__link" activeClassName="header--active" to="/">About</NavLink>
-    <a href="mailto:example@email.com" className="header__link header__email">travis@travisturner.io</a>
+    <NavLink className="header__link" activeClassName="header--active" to="/contact">Contact</NavLink>
+    <a href="mailto:travis@travisturner.io" className="header__link header__email">travis@travisturner.io</a>
   </div>
 );
 
@@ -13,5 +15,5 @@ export default NavBar;
 //<a className="header__link" href="#" target="_blank">Blog</a>
 /*
 <a href="#" className="header__link">CV</a>
-<NavLink className="header__link" activeClassName="header--active" to="/projects">Projects</NavLink>
+
 */
