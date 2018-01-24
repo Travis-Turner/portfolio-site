d3a089efe0d75d91f70c05f28c188705d07182e6
Travis Turner <roboenigma@gmail.com>
2018-01-23 20:59:39 -0500

    Add projects page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ const NavBar = () => (
   <div id="navbar__container">
   <a className="header__link" href="./TravisTurnerResume2018.pdf" target="_blank">CV</a>
     <NavLink exact className="header__link" activeClassName="header--active" to="/">About</NavLink>
+    <NavLink className="header__link" activeClassName="header--active" to="/projects">Projects</NavLink>
     <NavLink className="header__link" activeClassName="header--active" to="/contact">Contact</NavLink>
     <a href="mailto:travis@travisturner.io" className="header__link header__email">travis@travisturner.io</a>
   </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,43 @@
 import React from 'react';
 
-const Projects = () => (
-  <div>
-    Projects Page
-  </div>
-);
+class Projects extends React.Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      categoryHover: false
+    }
+  }
+  render (){
+    return (
+      <div>
+        <div id="projects__header">
+          <h1>PROJECTS</h1>
+        </div>
+        <div id="projects__container">
+          <div id="web-dev__container">
+              <a href="#" id="category-web" className="category-box category--link">
+                <h3 className="web-title">WEB</h3>
+                <h3 className="web-title" id="h3-dev">DEV</h3>
+              </a>
+          </div>
+          <div id="programming__container">
+            <a href="#" className="category-prog category-box category--link">
+              <h3 className="prog-title">PROGRAMMING</h3>
+            </a>
+          </div>
+          <div id="graphic-design__container">
+            <a href="#" className="category-graphic category-box category--link">
+              <div id="graphic-design__flex">
+                <h3 className="graphic-title">GRAPHIC</h3>
+                <h3 className="graphic-title" id="h3-design">DESIGN</h3>
+              </div>
+            </a>
+          </div>
+          
+        </div>
+      </div>
+    )
+  };
+}
 
 export default Projects;
